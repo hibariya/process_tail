bd3c9c352d9dfe96fe7bc8b39565bdf4a7e5d99c
Hika Hibariya <hibariya@gmail.com>
2015-02-02 23:44:40 +0900

    Check dependencies

--- ext/process_tail/extconf.rb
+++ ext/process_tail/extconf.rb
@@ -1,5 +1,13 @@
 require 'mkmf'
 
-abort 'missing ptrace' unless have_func('ptrace')
+%w(string.h sys/ptrace.h sys/syscall.h sys/types.h sys/user.h sys/wait.h ruby.h ruby/thread.h).each do |h|
+  abort "missing #{h}" unless have_header(h)
+end
+
+abort 'missing ptrace' unless have_func('ptrace', 'sys/ptrace.h')
+
+%w(orig_rax rdi rdx rsi rdx).each do |m|
+  abort "missing #{m}" unless have_struct_member('struct user_regs_struct', m, 'sys/user.h')
+end
 
 create_makefile 'process_tail/process_tail'
