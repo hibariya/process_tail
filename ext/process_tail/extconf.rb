ae96ae50a81d09ce8e74bf48f31df43b80a34f84
Hika Hibariya <hibariya@gmail.com>
2015-02-08 19:01:43 +0900

    Unnecessary

--- ext/process_tail/extconf.rb
+++ ext/process_tail/extconf.rb
@@ -4,10 +4,11 @@ require 'mkmf'
   abort "missing #{h}" unless have_header(h)
 end
 
-abort 'missing ptrace' unless have_func('ptrace', 'sys/ptrace.h')
-
 %w(orig_rax rdi rdx rsi rdx).each do |m|
   abort "missing #{m}" unless have_struct_member('struct user_regs_struct', m, 'sys/user.h')
 end
+
+abort 'missing __WALL'                unless have_const('__WALL', 'sys/wait.h')
+abort 'missing PTRACE_O_TRACESYSGOOD' unless have_const('PTRACE_O_TRACESYSGOOD', 'sys/ptrace.h')
 
 create_makefile 'process_tail/process_tail'
